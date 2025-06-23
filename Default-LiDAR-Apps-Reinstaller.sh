3c55810fdb847dcded99fdfc95b7798c4d27762f
PurpleRabbit75 <140262297+PurpleRabbit75@users.noreply.github.com>
2025-06-23 15:55:04 -0500

    Added a script to reinstall the necessary baseline apps after a Ubuntu reinstall

---
+++ Default-LiDAR-Apps-Reinstaller.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+sudo apt install htop &
+sudo apt install openssh-server &
+sudo apt install blender &
+sudo apt install snapd
+sudo snap install --classic code
+sudo snap install cloudcompare
+
+
