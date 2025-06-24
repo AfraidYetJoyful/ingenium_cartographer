e7fe971ab3cfa8b97f48bc032188e0694c4ff309
PurpleRabbit75 <140262297+PurpleRabbit75@users.noreply.github.com>
2025-06-24 12:50:00 -0500

    add spaces

--- Default-LiDAR-Apps-Reinstaller.sh
+++ Default-LiDAR-Apps-Reinstaller.sh
@@ -5,8 +5,12 @@ sudo apt upgrade
 sudo apt install htop &
 sudo apt install openssh-server &
 sudo apt install blender &
+
 sudo apt install snapd
-sudo snap install --classic code
-sudo snap install cloudcompare
+sudo snap install --classic code &
+sudo snap install cloudcompare &
+
+sudo apt install git-all
+
 
 
