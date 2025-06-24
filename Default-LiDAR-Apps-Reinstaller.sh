110e6f5901d309ab6d274af39d072d958b2f4662
PurpleRabbit75 <140262297+PurpleRabbit75@users.noreply.github.com>
2025-06-24 11:34:53 -0500

    Added comments and a git installer command

--- Default-LiDAR-Apps-Reinstaller.sh
+++ Default-LiDAR-Apps-Reinstaller.sh
@@ -2,11 +2,12 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install htop
-sudo apt install openssh-server
-sudo apt install blender
-sudo apt install snapd
-sudo snap install --classic code
-sudo snap install cloudcompare
+sudo apt install htop # Disk space monitor
+sudo apt install openssh-server # SSH client
+sudo apt install blender # Install blender (a 3D modeling software)
+sudo apt install snapd # A package manager
+sudo snap install --classic code # Visual Studio Code, a git-integrated IDE for basically all computer languages
+sudo snap install cloudcompare # Install CloudCompare (a point-cloud processing software)
+sudo apt-get install git # Install git (a source control software for coders)
 
 
