3b6bf816cc83f99dd7d8fa3d529cb357b25db1ff
PurpleRabbit75 <140262297+PurpleRabbit75@users.noreply.github.com>
2025-06-24 10:58:19 -0500

    Update Default app reinstaller to remove & at line ends
    The &s were causing scripts that required Y/n prompts to be skipped and never be authenticated as the terminal ran on without them. I therefore removed the &s, which caused the script to run correctly.

--- Default-LiDAR-Apps-Reinstaller.sh
+++ Default-LiDAR-Apps-Reinstaller.sh
@@ -2,9 +2,9 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install htop &
-sudo apt install openssh-server &
-sudo apt install blender &
+sudo apt install htop
+sudo apt install openssh-server
+sudo apt install blender
 sudo apt install snapd
 sudo snap install --classic code
 sudo snap install cloudcompare
