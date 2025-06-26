0915c521c57b6ab0542f8aad602f1306b496d6b4
Ingenium-LiDAR <ingenium.lidar@outlook.com>
2025-06-26 14:45:48 -0500

    Created script to install ROS Jazzy and edited Default-LiDAR-Apps-Reinstaller to include instructions to execute it

--- Default-LiDAR-Apps-Reinstaller.sh
+++ Default-LiDAR-Apps-Reinstaller.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-# Bash script to reinstall all the scripts and apps necessary for the project. Run on a fresh Ubuntu install.
+#AB Bash script to reinstall all the scripts and apps necessary for the project. Run on a fresh Ubuntu install.
 
 sudo apt update
 sudo apt upgrade
-sudo apt install htop # Disk space monitor
-sudo apt install openssh-server # SSH client
-sudo apt install blender # Install blender (a 3D modeling software)
-sudo apt install snapd # A package manager
-sudo snap install --classic code # Visual Studio Code, a git-integrated IDE for basically all computer languages
-sudo snap install cloudcompare # Install CloudCompare (a point-cloud processing software)
-sudo apt-get install gnome-keyring # Install a secure cryptographic library needed by VS Code
-sudo apt install rpi-imager # a software for burning OSes onto SD cards for use in a Raspberry Pi
-sudo snap refresh firefox # Update the default-installed firefox to the latest version
+sudo apt install htop #AB Disk space monitor
+sudo apt install openssh-server #AB SSH client
+sudo apt install blender #AB Install blender (a 3D modeling software)
+sudo apt install snapd #AB A package manager
+sudo snap install --classic code #AB Visual Studio Code, a git-integrated IDE for basically all computer languages
+sudo snap install cloudcompare #AB Install CloudCompare (a point-cloud processing software)
+sudo apt-get install gnome-keyring #AB Install a secure cryptographic library needed by VS Code
+sudo apt install rpi-imager #AB a software for burning OSes onto SD cards for use in a Raspberry Pi
+sudo snap refresh firefox #AB Update the default-installed firefox to the latest version
 
-sudo apt-get install git # Install and then configure git (a source control software for coders)
+sudo apt-get install git #AB Install and then configure git (a source control software for coders)
 git config --global user.email "ingenium.lidar@outlook.com"
 git config --global user.name "Ingenium-LiDAR"
 
-sudo apt-get install gnome-keyring # Install a secure cryptographic library needed by VS Code
+sudo apt-get install gnome-keyring #AB Install a secure cryptographic library needed by VS Code
 
-sudo apt autoremove # Remove all files not needed in the system. Frees up a variable amount of space (on the Jun 24, 2025 reinstall, I had superfluous firmware. You never know...)
+sudo apt autoremove #AB Remove all files not needed in the system. Frees up a variable amount of space (on the Jun 24, 2025 reinstall, I had superfluous firmware. You never know...)
+
+if [ -f "./Install_Jazzy.sh" ]; #AB If file exists...
+then 
+  ./Install_Jazzy.sh #AB ...execute it
+else #AB otherwise skip it.
+    echo "./Install_Jazzy.sh was not found. Skipping ROS Jazzy install..."
+fi
+
---
+++ Install_Jazzy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#AB ROS Jazzy Installation Script
+#AB to use ROS in a given terminal session, run source /opt/ros/jazzy/setup.bash
+
+echo "Updating apt..."
+sleep 1
+sudo apt update
+sudo apt upgrade
+sudo apt autoremove
+
+echo "Installing universe repository..."
+sleep 1
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+echo "Configuring system..."
+sleep 1
+sudo apt update && sudo apt install curl -y
+export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $UBUNTU_CODENAME)_all.deb" # If using Ubuntu derivates use $UBUNTU_CODENAME
+sudo dpkg -i /tmp/ros2-apt-source.deb
+
+echo "Updating apt..."
+sleep 1
+sudo apt update
+sudo apt upgrade
+
+echo "Installing ros-jazzy-desktop..."
+sleep 1
+sudo apt install ros-jazzy-desktop
+
+echo "ROS2 Jazzy installation complete."
+sleep 1
+
