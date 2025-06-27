40c40693fcfd8844760d2e2871bb5d4e58f1af90
Ingenium-LiDAR <ingenium.lidar@outlook.com>
2025-06-27 16:36:21 -0500

    added newlines to echo commands

--- Default_Apps_Installer.sh
+++ Default_Apps_Installer.sh
@@ -5,7 +5,8 @@
 
 
 #---------------------------------------------INSTALL BASIC PACKAGES---------------------------------------------
-
+echo -ne "Installing base packages...\n"
+sleep 1
 
 sudo apt update
 sudo apt upgrade
@@ -53,5 +54,5 @@ sudo apt-get install ros-jazzy-microstrain-inertial-driver #AB Install the IMU d
 #---------------------------------------------CLEANUP---------------------------------------------
 
 
-echo "Running sudo apt autoremove:"
+echo -ne "Running sudo apt autoremove:\n"
 sudo apt autoremove #AB Remove all files not needed in the system. Frees up a variable amount of space (on the Jun 24, 2025 reinstall, I had superfluous firmware. You never know...)
