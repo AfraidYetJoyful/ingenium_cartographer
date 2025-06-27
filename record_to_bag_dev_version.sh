fe129f8c69ac829d82cf2c5cc795b3bd0c776ad1
Ingenium-LiDAR <ingenium.lidar@outlook.com>
2025-06-27 16:25:55 -0500

    changed a few things to try to make it work with ros2. Partial success.

--- record_to_bag_dev_version.sh
+++ record_to_bag_dev_version.sh
@@ -1,13 +1,13 @@
 source /opt/ros/jazzy/setup.bash
 
 # Start the connection to the lidar
-rosrun velodyne_driver device_ip=192.168.1.201 model=32C rpm=600 &
+ros2 run velodyne_driver device_ip=192.168.1.201 model=32C rpm=600 &
 sleep 2
 
 # Start recording specific nodes from the lidar and the imu
-ros2 bag record /velodyne_packets &
+ros2 bag record --all &
 sleep 2
 
 echo "Currently recording, press any key to exit"
 read -r ### Wait for an input of any key, then proceed to the next line
-rosnode kill -a ### Kills all active ROS nodes
+ros2 node kill -a ### Kills all active ROS nodes
