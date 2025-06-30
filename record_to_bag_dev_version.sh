5d7234f2cd1ecd895dea0a2e19af2b1ba3448747
Ingenium-LiDAR <ingenium.lidar@outlook.com>
2025-06-30 14:14:46 -0500

    Added script to move directories full of .bag files out of the directory in which the script is running into a directory called "Data", within /home/lidar/Documents

--- record_to_bag_dev_version.sh
+++ record_to_bag_dev_version.sh
@@ -1,9 +1,12 @@
 source /opt/ros/jazzy/setup.bash
 
+cwd=$(pwd)
+ethernet=enp152s0
+
 #AB Configure the IP address of the ethernet port to recieve data from the default IP of a VLP-32C. Replace enp152s0 with the name of your ethernet port, which can be found using ip addr 
-sudo ip addr flush dev enp152s0
-sudo ip addr add 192.168.1.100/24 dev enp152s0 
-sudo ip route add 192.168.1.201 dev enp152s0
+sudo ip addr flush dev $ethernet
+sudo ip addr add 192.168.1.100/24 dev $ethernet 
+sudo ip route add 192.168.1.201 dev $ethernet
 
 
 # Start the connection to the lidar
@@ -25,3 +28,11 @@ sleep 2
 echo "Currently recording, press any key to exit"
 read -r ### Wait for an input of any key, then proceed to the next line
 ros2 node kill -a ### Kills all active ROS nodes
+
+for dir in "$(pwd)"/*/     #AB Iterate through every directory in the current working directory (i.e., the directory in which the script is running)
+do
+    if [[ "$dir" =~ rosbag2_* ]]; then #AB Use a regular expression to determien if any of the directories starts with the phrase "rosbag2_" and has other characters after it
+        mv "$dir" "/home/lidar/Documents/Data" #AB If it matches this pattern, move it to the Data folder out of the script folder
+    fi
+
+done
