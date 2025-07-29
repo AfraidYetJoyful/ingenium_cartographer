777137abe6a6308c846382441692b9b60a7a43b1
Ingenium-LiDAR <ingenium.lidar@outlook.com>
2025-07-29 10:29:37 -0500

    Moved docker installation from DAI to ILS

--- Install_LIO-SAM.sh
+++ Install_LIO-SAM.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-# Generative AI was used to create some portions of this script.
-
 #AB LIO-SAM Installation Script. Installs from https://github.com/TixiaoShan/LIO-SAM/tree/ros2
 
 
 sudo apt update
 sudo apt ugrade
 sudo apt autoremove
+
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+
+curl "https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb" --output docker-desktop-amd64.deb
+sudo apt-get install ./docker-desktop-amd64.deb #AB Install Docker Desktop. This is required to run LIO-SAM.
+
+
 
 CWD=$(pwd) #AB store the current directory
 
