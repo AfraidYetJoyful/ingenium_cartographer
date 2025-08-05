52f4a5074b007b0e66ddfdca6be7a1281dc7c80e
Ingenium-LiDAR <ingenium.lidar@outlook.com>
2025-08-05 17:11:01 -0500

    Added end-of-file pause

--- process_bag.sh
+++ process_bag.sh
@@ -2,6 +2,7 @@
 
 source /opt/ros/humble/setup.bash
 
+cwd=$(pwd) #AB save the current working directory
 file=$(realpath "$1")
 
 ros2 launch lio_sam run.launch.py &
@@ -10,3 +11,9 @@ sleep 5
 cd /home/Documents/Data
 
 ros2 bag play $file
+
+# Return to original directory
+cd "$cwd"
+
+echo "Bag fully processed, press enter to exit"
+read -r ### Pause until reading any character. Once a character is read, progress to the next line (that is, terminate the program).
