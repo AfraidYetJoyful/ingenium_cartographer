f4411297731363841892b1dab4fad7ddbb6277d9
Ingenium-LiDAR <ingenium.lidar@outlook.com>
2025-08-05 17:32:59 -0500

    Final text now prints in Bold Cyan style.

--- process_bag.sh
+++ process_bag.sh
@@ -2,8 +2,7 @@
 
 source /opt/ros/humble/setup.bash
 
-BOLD_CYAN='\e[1;36m'
-NC='\033[0m'
+
 
 cwd=$(pwd) #AB save the current working directory
 file=$(realpath "$1")
@@ -18,5 +17,5 @@ ros2 bag play $file --log-level debug
 # Return to original directory
 cd "$cwd"
 
-echo "process_bag.sh has finished running. Press enter to exit."
+echo -e ' \e[1;36mprocess_bag.sh has finished running. Press enter to exit.'
 read -r ### Pause until reading any character. Once a character is read, progress to the next line (that is, terminate the program).
