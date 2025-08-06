b92f484dce1ba3edc57a54415e152282e388dc78
Ingenium-LiDAR <ingenium.lidar@outlook.com>
2025-08-06 16:03:11 -0500

    Removed self-delete

--- Default_Apps_Installer.sh
+++ Default_Apps_Installer.sh
@@ -115,4 +115,4 @@ echo 'alias update="sudo apt update && sudo apt upgrade && sudo apt autoremove"'
 echo -ne "Running sudo apt autoremove:\n"
 sudo apt autoremove #AB Remove all files not needed in the system. Frees up a variable amount of space (on the Jun 24, 2025 reinstall, I had superfluous firmware. You never know...)
 
-echo 'Deleting file at $0' && rm "$0" #AB Move the currently running script to Trash, since a copy of it is now present in ingenium_cartographer.
+# echo 'Deleting file at $0' && rm "$0" #AB Move the currently running script to Trash, since a copy of it is now present in ingenium_cartographer.
