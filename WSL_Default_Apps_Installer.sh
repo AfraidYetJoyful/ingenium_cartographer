3720258d134bc695bb5e413ede5dbb15b290f55b
Ingenium-LiDAR <ingenium.lidar@outlook.com>
2025-08-06 15:44:47 -0500

    Added self-remove commands to DAI and WSLDAI

--- WSL_Default_Apps_Installer.sh
+++ WSL_Default_Apps_Installer.sh
@@ -16,3 +16,5 @@ fi
 cd ingenium_cartographer #AB Enter the newly cloned repository
 chmod +x Default_Apps_Installer.sh #AB Mark DAI as executable
 ./Default_Apps_Installer.sh #AB run DAI
+
+echo 'Deleting $(basename "$0")' && rm $(basename "$0") #AB Move the currently running script to Trash, since a copy of it is now present in ingenium_cartographer.
