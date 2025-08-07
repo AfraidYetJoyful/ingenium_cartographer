7e18d3c59cbdc5a15f9029dedafaaad4e39543a6
Ingenium-LiDAR <ingenium.lidar@outlook.com>
2025-08-07 11:33:51 -0500

    WSLDAI now explicitly switches to humble branch every time. Added no-color sequences to the ends of all the echoes

--- agent_scripts/Default_Apps_Installer.sh
+++ agent_scripts/Default_Apps_Installer.sh
@@ -13,14 +13,14 @@ BOLD_CYAN='\e[1;36m'
 #---------------------------------------------INSTALL BASIC PACKAGES---------------------------------------------
 
 
-echo -e "\e[38;5;82mInstalling base packages..."
+echo -e "\e[38;5;82mInstalling base packages...\033[0m"
 sleep 1
 
-echo -e "\e[38;5;82mUpdating apt..."
+echo -e "\e[38;5;82mUpdating apt...\033[0m"
 sudo apt update
 sudo apt upgrade
 
-echo -e "\e[38;5;82mInstalling htop, openss-server, gnome-keyring, gnome-tweaks, snapd, yamllint, sl, and colcon..."
+echo -e "\e[38;5;82mInstalling htop, openss-server, gnome-keyring, gnome-tweaks, snapd, yamllint, sl, and colcon...\033[0m"
 sudo apt install htop #AB Disk space monitor
 sudo apt install openssh-server #AB SSH client
 sudo apt-get install gnome-keyring #AB Install a secure cryptographic library needed by VS Code
@@ -31,14 +31,14 @@ sudo apt install sl #AB Install sl, an alias for ls
 sudo apt install python3-colcon-core #Install colcon, the build manager for ROS2
 
 
-echo -e "\e[38;5;82mInstalling VS Code, Firefox, CloudCompare, and Blender..."
+echo -e "\e[38;5;82mInstalling VS Code, Firefox, CloudCompare, and Blender...\033[0m"
 sudo snap install --classic code #AB Visual Studio Code, a git-integrated IDE for basically all computer languages
 sudo snap install firefox
 sudo snap refresh firefox #AB Update the default-installed firefox to the latest version
 sudo snap install cloudcompare #AB Install CloudCompare (a point-cloud processing software)
 sudo snap install --classic blender #AB Install blender (a 3D modeling software)
 
-echo -e "\e[38;5;82mInstalling git and configuring git and VS Code..."
+echo -e "\e[38;5;82mInstalling git and configuring git and VS Code...\033[0m"
 sudo apt-get install git #AB Install and then configure git (a source control software for coders)
 git config --global user.email "ingenium.lidar@outlook.com"
 git config --global user.name "Ingenium-LiDAR"
@@ -50,13 +50,13 @@ code --password-store="gnome-libsecret" #AB Configure VS Code to use Gnome Keyri
 #---------------------------------------------INSTALL "ingenium_cartographer" REPOSITORY---------------------------------------------
 
 
-echo -e "\e[38;5;82mSetting up the default directory structure..."
+echo -e "\e[38;5;82mSetting up the default directory structure...\033[0m"
 mkdir -p ~/Documents/GitHub
 mkdir -p ~/Documents/Data
 mkdir ~/Apps
 
 
-echo -e "\e[38;5;82mInstalling ingenium_cartographer repository..."
+echo -e "\e[38;5;82mInstalling ingenium_cartographer repository...\033[0m"
 if ! [ -d ~/Documents/GitHub/ingenium_cartographer ]; then #AB If a directory called ingenium_cartographer does not already exist in ~/Documents/GitHub...
   cd ~/Documents/GitHub #AB ...navigate to the ~/Documents/GitHub directory
   git clone https://github.com/JohannesByle/ingenium_cartographer
@@ -83,7 +83,7 @@ gsettings set org.gnome.desktop.background picture-uri file:~/Documents/GitHub/i
 #---------------------------------------------INSTALL ROS2 Jazzy---------------------------------------------
 
 
-echo -e "\e[38;5;82mInstalling ROS2 Humble..."
+echo -e "\e[38;5;82mInstalling ROS2 Humble...\033[0m"
 cd ~/Documents/GitHub/ingenium_cartographer/agent_scripts #AB Navigate to the ingenium_cartographer/agent_scripts directory. 
 ./Install_Humble.sh #AB Run the Install_Jazzy.sh script to install ROS Jazzy 
 
@@ -93,9 +93,9 @@ cd ~/Documents/GitHub/ingenium_cartographer/agent_scripts #AB Navigate to the in
 
 
 cd ~/Documents/GitHub/ingenium_cartographer/agent_scripts #AB Navigate to the ingenium_cartographer/agent_scripts directory. Technically unnecessary at this stage since the script is already there, but best to make it explicit where the program needs to be.
-echo -e "\e[38;5;82mInstalling Docker and NVIDIA Container Toolkit..."
+echo -e "\e[38;5;82mInstalling Docker and NVIDIA Container Toolkit...\033[0m"
 ./Install_Docker.sh #FK Run the Install_Docker.sh script to install docker
-echo -ne "Installing NVIDIA Container Toolkit...\n"
+echo -e "\e[38;5;82mInstalling NVIDIA Container Toolkit...\033[0m"
 ./Install_NVIDIA_Docker_Tools.sh #FK Run the Install_NVIDIA_Docker_Tools.sh to install NVIDIA Container toolkit
 
 
@@ -103,7 +103,7 @@ echo -ne "Installing NVIDIA Container Toolkit...\n"
 #---------------------------------------------INSTALL LIO-SAM---------------------------------------------
 
 
-echo -e "\e[38;5;82mInstalling LIO-SAM..."
+echo -e "\e[38;5;82mInstalling LIO-SAM...\033[0m"
 if ! [ -d ~/Apps/LIO-SAM ]; then #AB If a directory called LIO-SAM is not already in the ~/Apps directory...
   cd ~/Documents/GitHub/ingenium_cartographer/agent_scripts #AB ...navigate to the ingenium_cartographer directory
   ./Finns_Install_LIO-SAM.sh #FK temp LIO-SAM installer + docker container launch file #AB Installs inside the ~/APps directory
@@ -114,11 +114,11 @@ fi
 #---------------------------------------------CLEANUP---------------------------------------------
 
 
-echo -e "\e[38;5;82mCleaning up..."
+echo -e "\e[38;5;82mCleaning up...\033[0m"
 echo 'alias cleanup="./cleanup.sh"' >> ~/.bashrc #AB add the alias cleanup to the system ~/.bashrc file. It will now run ./cleanup.sh whenever the command "cleanup" is entered.
 echo 'alias update="sudo apt update && sudo apt upgrade && sudo apt autoremove"' >> ~/.bashrc #AB add the alias update to the system ~/.bashrc file. It will now update, upgrade, and finally autoremove all unnecessary files whenever the command "update" is entered.
 
 echo -ne "Running sudo apt autoremove:\n"
 sudo apt autoremove #AB Remove all files not needed in the system. Frees up a variable amount of space (on the Jun 24, 2025 reinstall, I had superfluous firmware. You never know...)
 
-echo -e "\e[38;5;82mDefault_Apps_Installer.sh has finished running now."
+echo -e "\e[38;5;82mDefault_Apps_Installer.sh has finished running now.\033[0m"
--- agent_scripts/WSL_Default_Apps_Installer.sh
+++ agent_scripts/WSL_Default_Apps_Installer.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-echo -e "\e[38;5;82mUpdating apt..."
+echo -e "\e[38;5;82mUpdating apt...\033[0m"
 sudo apt update
 sudo apt upgrade
 sudo apt autoremove
 
-echo -e "\e[38;5;82mInstalling Gnome Desktop..."
+echo -e "\e[38;5;82mInstalling Gnome Desktop...\033[0m"
 sudo apt install tasksel
 sudo tasksel install ubuntu-desktop
 sudo apt install gnome-tweaks
 
-echo -e "\e[38;5;82mInstalling ingenium_cartographer repository..."
+echo -e "\e[38;5;82mInstalling ingenium_cartographer repository...\033[0m"
 if ! [ -d ~/Documents/GitHub/ingenium_cartographer ]; then #AB If a directory called ingenium_cartographer does not already exist in ~/Documents/GitHub...
   mkdir -p ~/Documents/GitHub && cd ~/Documents/GitHub #AB then make any parts of that path that do not already exist and navigate to the GitHub directory
   git clone https://github.com/JohannesByle/ingenium_cartographer #AB Clone the ingenium_cartographer repository
-  git switch humble #AB Switch to the humble branch of the ingenium_cartographer repository
 fi
 
-cd ingenium_cartographer/agent_scripts #AB Enter the newly cloned repository
+cd ~/Documents/GitHub/ingenium_cartographer #AB Enter the newly cloned repository
+git switch humble #AB Switch to the humble branch of the ingenium_cartographer repository
+cd ~/Documents/GitHub/ingenium_cartographer/agent_scripts 
 chmod +x Default_Apps_Installer.sh #AB Mark DAI as executable
-echo -e "\e[38;5;82mRunning Default_Apps_Installer.sh..."
+echo -e "\e[38;5;82mRunning Default_Apps_Installer.sh...\033[0m"
 ./Default_Apps_Installer.sh #AB run DAI
 
